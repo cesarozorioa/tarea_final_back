9e7a3d33f95ad6569c739922e03b7c07e7f5ea7d
Cesar Osorio <cesarozorioa@hotmail.com>
2024-06-15 02:50:22 -0500

    progreso de tareas

--- App1/urls.py
+++ App1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from App1.views import IndexReview,ProyectoView
+from App1.views import IndexReview,ProyectoView,actualizar_item
 
-urlpatterns = [   
+urlpatterns = [  
+    path('actualizar-item/<int:item_id>/', actualizar_item, name='actualizar_item'), 
     path('',IndexReview),
     path('proyectos/<int:id>',ProyectoView),
 ]
--- App1/views.py
+++ App1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render,redirect
 from django.http import HttpResponse
 #para hacer consulta a las bases de datos
 from .models import ProyectoDb,TareaDb
@@ -11,5 +11,13 @@ def IndexReview(request):
 
 def ProyectoView(request,id):
     proyecto = get_object_or_404(ProyectoDb,id=id)
-    return render(request,'proyectos.html',{"objeto":proyecto})
+    return render(request,'tareas.html',{"objeto":proyecto})
 
+def actualizar_item(request, item_id):
+    item = get_object_or_404(TareaDb, id=item_id)
+    if request.method == 'POST':
+        # Actualizamos el dato, por ejemplo incrementando la cantidad
+        item.done = True
+        item.save()
+        return redirect(IndexReview)
+    return HttpResponse(status=405)
